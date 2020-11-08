dcf9b4c468fa3c3daa8b108491e71ffdb1d93b5b
Anton <osipchika@gmail.com>
2020-11-08 22:43:36 +0300

    сверстал header and banner with media

--- build/js/main.js
+++ build/js/main.js
@@ -1,25 +1,36 @@
-// const header = document.querySelector('.header');
-// const bannerSection = document.querySelector('.banner__section');
-// const bannerSectionHeight = bannerSection.clientHeight;
-
-// let timerId = setInterval(headerScroll, 100);
-
-// function headerScroll () {
-//      let scrollHeight = window.pageYOffset;
-//      if(scrollHeight >= bannerSectionHeight-235){
-//           header.classList.add('header__scroll')
-//      }else{
-//           header.classList.remove('header__scroll')
-//      }
-//      console.log(scrollHeight);
-// }
+const menu = document.querySelector('.header__menu');
+const headerNavContainer = document.querySelector('.header__nav-container');
 
 
-const headerMenu = document.querySelector('.header__menu');
-const headerMainWrapper = document.querySelector('.header__main-wrapper');
-
-headerMenu.addEventListener('click',()=>{
-     headerMainWrapper.classList.toggle('header__main-wrapper--active');
-     headerMenu.classList.toggle('header__menu--active');
+menu.addEventListener('click',()=>{
+     menu.classList.toggle('header__menu--active')
+     headerNavContainer.classList.toggle('header__nav-container--active')
 })
+
+
+const header = document.querySelector('.header');
+const headerHight = header.clientHeight;
+
+let timerId = setInterval(() => showHeader(), 300);
+
+function showHeader(){
+     const windowScroll =  window.scrollY;
+     if(windowScroll>=headerHight){
+          header.classList.add('header--active')
+
+     }else{
+          header.classList.remove('header--active')
+
+     }
+}
+
+
+const cards = document.querySelectorAll('.services__cards');
+
+const arrows = document.querySelectorAll('.services__card-arrow');
+
+function getArrow(e){
+     
+}
+
 
