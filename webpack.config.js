7a0b821a367d710ba75f1b4fed2d9cfee0574807
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2021-09-10 16:50:14 +0300

    создал сборку

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path')
+const webpack = require('webpack')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+// const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+	context: path.resolve(__dirname, 'src'),
+	mode: 'development',
+	entry: {
+		main: ['@babel/polyfill', './index.js'],
+	},
+	output: {
+		filename: '[name].[contenthash].js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	devServer: {
+		historyApiFallback: true,
+		contentBase: path.resolve(__dirname, './dist'),
+		open: true,
+		compress: true,
+		hot: true,
+		port: 8080,
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			title: 'Paternity',
+			template: path.resolve(__dirname, './src/index.html'),
+		}),
+		new CleanWebpackPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		// new CopyWebpackPlugin({
+		// 	patterns: [
+		// 		{
+		// 			from: path.resolve(__dirname, './src/assets/img/ouch2.svg'),
+		// 			to: path.resolve(__dirname, './dist'),
+		// 		},
+		// 	],
+		// }),
+		new MiniCssExtractPlugin({
+			filename: '[name].[contenthash].css',
+		}),
+	],
+	module: {
+		rules: [
+			// scss
+			{
+				test: /\.s[ac]ss$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					'css-loader',
+					'sass-loader',
+				],
+			},
+			// изображения
+			{
+				test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+				type: 'asset/resource',
+			},
+			// шрифты и SVG
+			{
+				test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+				type: 'asset/inline',
+			},
+			// JavaScript
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+			},
+		],
+	},
+}
